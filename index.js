b56a20f774c449ff3947755e374294d153da2688
Cristopher231 <crisperezcastro231@gmail.com>
2024-12-18 11:15:44 -0600

    Update index.js

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ const rl = createInterface(process.stdin, process.stdout)
 //const app = express()
 //const port = process.env.PORT || 8080;
 
-say('Hetero-\nBot-\nMD', {
+say('Gata\nBot\nMD', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
-say(`Por Cristopher231`, {
+say(`Por GataDios`, {
 font: 'console',
 align: 'center',
 gradient: ['red', 'magenta']})
